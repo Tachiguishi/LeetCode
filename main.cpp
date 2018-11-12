0bb10ef6a4148ba1ca6ba0cbc632413031a0385f
RorSpike <TachiguishiRor@gmail.com>
2018-11-12 18:59:49 +0800

    test: 添加164 test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <solution.h>
 #include <gtest/gtest.h>
 
-TEST(FooTest, Test1) {
-    EXPECT_EQ(1, 1);
-    EXPECT_TRUE(3 > 0);
+TEST(MaximumGap164, Test1) {
+	Solution sol;
+	vector<int> nums {3,6,9,1};
+	int res = sol.maximumGap(nums);
+    EXPECT_EQ(res, 3);
+}
+
+TEST(MaximumGap164, Test2) {
+	Solution sol;
+	vector<int> nums {10};
+	int res = sol.maximumGap(nums);
+    EXPECT_EQ(res, 0);
 }
 
 int main(int argc, char **argv) {
