a4eb34a98996c8167a89d3ee3a4642b5bb10d0c9
RorSpike <TachiguishiRor@gmail.com>
2018-11-23 17:08:41 +0800

    feat: 166 Accepted c++

--- src/166.fraction_to_recurring_decimal.cpp
+++ src/166.fraction_to_recurring_decimal.cpp
@@ -1,7 +1,78 @@
 #include <solution.h>
+#include <climits>
+#include <cmath>
+#include <map>
 
 string Solution::fractionToDecimal(int numerator, int denominator){
 	if(0 == denominator) return "NaN";
+	if(0 == numerator) return "0";
 	string res;
+	int quotient(0);
+	int remainder(0);
+	if(INT_MIN == numerator && (denominator == -1 || denominator == 1)){
+		quotient = numerator;
+	}
+	else{
+		quotient = numerator / denominator;
+		remainder = std::abs(numerator % denominator);
+	}
+
+	if(0 == quotient){
+		if((numerator > 0 && denominator < 0)
+			|| (numerator < 0 && denominator > 0)){
+				res.push_back('-');
+			}
+		res.push_back('0');
+	}
+	else{
+		bool negative(false);
+		if(quotient < 0 && -1 != denominator){
+			negative = true;
+		}
+		while(quotient){
+			int f = quotient % 10;
+			res.push_back(std::abs(f) + '0');
+			quotient = quotient / 10;
+		}
+		if(negative) res.push_back('-');
+		for(int i = 0; i < (int)res.size()/2; ++i){
+			std::swap(res[i], res[res.size()-1-i]);
+		}
+	}
+	if(0 != remainder){
+		res.push_back('.');
+	}
+	else{
+		return res;
+	}
+
+	bool irrational(false);
+	int tailSize(0);
+	long long lnumerator;
+	std::map<int,int> remainders;
+	string resTail;
+	while(remainder){
+		if(remainders.count(remainder)){
+			tailSize = remainders[remainder];
+			irrational = true;
+			break;
+		}
+
+		remainders[remainder] = tailSize++;
+
+		lnumerator = (long long)remainder * 10;
+		quotient = lnumerator / denominator;
+		remainder = std::abs(lnumerator % denominator);
+
+		resTail.push_back(std::abs(quotient) + '0');
+	}
+
+	res.append(resTail.substr(0, tailSize));
+	if(irrational){
+		res.push_back('(');
+		res.append(resTail.substr(tailSize));
+		res.push_back(')');
+	}
+
 	return res;
 }
--- test/testcase166.h
+++ test/testcase166.h
@@ -23,6 +23,8 @@ TEST_P(FractionToDecimal166, test1){
 
 INSTANTIATE_TEST_CASE_P(leetcode, FractionToDecimal166,
 	::testing::Values(TwoIntToString(1,2,"0.5"),
+	TwoIntToString(-2147483648, 1, "-2147483648"),
+	TwoIntToString(-1, -2147483648, "0.0000000004656612873077392578125"),
 	TwoIntToString(2,1,"2"),
 	TwoIntToString(2,3,"0.(6)"),
 	TwoIntToString(1,8,"0.125"),
@@ -30,8 +32,11 @@ INSTANTIATE_TEST_CASE_P(leetcode, FractionToDecimal166,
 	TwoIntToString(1,0,"NaN"),
 	TwoIntToString(-2,1,"-2"),
 	TwoIntToString(2,-3,"-0.(6)"),
-	TwoIntToString(-1,8,"-0.125"),
+	TwoIntToString(-2,3,"-0.(6)"),
+	TwoIntToString(-9,8,"-1.125"),
+	TwoIntToString(9,-8,"-1.125"),
 	TwoIntToString(67,-6,"-11.1(6)"),
 	TwoIntToString(-1,0,"NaN"),
+	TwoIntToString(378261,3, "126087"),
 	TwoIntToString(INT_MIN, -1,"2147483648")
 	));
