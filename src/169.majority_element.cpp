29cd3ddfd1a5965d8752cb52f2f26273f9d21642
RorSpike <TachiguishiRor@gmail.com>
2018-12-05 18:50:03 +0800

    feat: 169 accepted c++

--- src/169.majority_element.cpp
+++ src/169.majority_element.cpp
@@ -1,5 +1,17 @@
 #include <solution.h>
 
 int Solution::majorityElement(vector<int>& nums){
-	return 0;
+	std::map<int, int> numCount;
+	for(int i : nums){
+		numCount[i]++;
+	}
+	int res = nums[0];
+	int count = 1;
+	for(auto& item : numCount){
+		if(item.second > count){
+			count = item.second;
+			res = item.first;
+		}
+	}
+	return res;
 }
