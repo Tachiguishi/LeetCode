0108cd371f0ca1e64867b6f260fe7ee74e612266
RorSpike <TachiguishiRor@gmail.com>
2018-12-12 19:33:01 +0800

    feat: 172 accepted c++

--- src/172.factorialTrailingZeroes.cpp
+++ src/172.factorialTrailingZeroes.cpp
@@ -1,5 +1,11 @@
 #include <solution.h>
 
 int Solution::trailingZeroes(int n){
-	return 0;
+	int count(0);
+	int i(5);
+	while(n > 0){
+		count += n / i;
+		n = n / 5;
+	}
+	return count;
 }
--- test/testcase172.h
+++ test/testcase172.h
@@ -3,8 +3,8 @@
 
 TEST(FactorialTrailingZeroes172, test1){
 	Solution sol;
-	vector<int> input = {3, 5, 9, 10, 20, 50, 100, 134};
-	vector<int> output = {0, 1, 1, 2, 4, 12, 24, 32};
+	vector<int> input = {3, 5, 9, 10, 20, 50, 100, 134, 142, 999, 9999, 99999};
+	vector<int> output = {0, 1, 1, 2, 4, 12, 24, 32, 34, 246, 2495, 24994};
 	for(size_t i = 0; i < input.size(); ++i){
 		EXPECT_EQ(sol.trailingZeroes(input[i]), output[i]) << "input: " << input[i];
 	}
