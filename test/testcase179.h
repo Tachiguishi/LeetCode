2d01a3155f6119c9c10f9f225faea5b2cf52fb5f
RorSpike <TachiguishiRor@gmail.com>
2018-12-25 19:37:06 +0800

    test: add 179 testcase

--- test/testcase179.h
+++ test/testcase179.h
@@ -21,5 +21,6 @@ INSTANTIATE_TEST_CASE_P(leetcode, LargestNumber179,
 	::testing::Values(TwoString("[10,2]", "210"),
 		TwoString("[1,2,3,4,5,6,7,8,9,0]", "9876543210"),
 		TwoString("[0,0]", "0"),
+		TwoString("[824,938,1399,5607,6973,5703,9609,4398,8247]", "9609938824824769735703560743981399"),
 		TwoString("[3,30,34,9,5]", "9534330")
 	));
