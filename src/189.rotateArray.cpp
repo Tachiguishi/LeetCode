66d866d418afaec845d02495c61391d535cb2d21
RorSpike <TachiguishiRor@gmail.com>
2019-01-17 18:40:43 +0800

    feat(189): accepted c++

--- src/189.rotateArray.cpp
+++ src/189.rotateArray.cpp
@@ -1,5 +1,15 @@
 #include <solution.h>
 
 void Solution::rotate(vector<int>& nums, int k){
+	size_t n = nums.size();
+	if(n <= 1) return;
 
+	k = k % n;
+	for(int i = 1; i <= k; ++i){
+		int end = nums[n - 1];
+		for(size_t j = n - 1; j > 0; --j){
+			nums[j] = nums[j - 1];
+		}
+		nums[0] = end;
+	}
 }
