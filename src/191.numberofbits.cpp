e55598852ea2e01766f10f3226c24f559b513658
RorSpike <TachiguishiRor@gmail.com>
2019-01-21 19:42:41 +0800

    feat(191): accepted c++

--- src/191.numberofbits.cpp
+++ src/191.numberofbits.cpp
@@ -1,5 +1,11 @@
 #include <solution.h>
 
 int Solution::hammingWeight(uint32_t n){
-	return n;
+	int res(0);
+	for(uint32_t i = 0; i < 32; ++i){
+		if(n & (1 << i)){
+			++res;
+		}
+	}
+	return res;
 }
