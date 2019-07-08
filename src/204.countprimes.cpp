3f06405a102ec0d9a7e36a990c062b777a65db27
RorSpike <TachiguishiRor@gmail.com>
2019-07-08 10:51:00 +0800

    feat(204): accepted c++

--- src/204.countprimes.cpp
+++ src/204.countprimes.cpp
@@ -1,5 +1,20 @@
 #include <solution.h>
 
 int Solution::countPrimes(int n){
-	return 0;
+	vector<bool> primes(n, true);
+	for(int i = 2; i * i < n; i++){
+		if(!primes[i]){
+			continue;
+		}
+
+		for(int j = i * i; j < n; j += i){
+			primes[j] = false;
+		}
+	}
+
+	int count(0);
+	for(int i = 2; i < n; i++){
+		if(primes[i]) count++;
+	}
+	return count;
 }
